2792b1907abafe4373a1a789b966134d0e3cad2f
Emily.N <emily.nowakovki@smail.inf.h-brs.de>
2024-10-06 12:58:16 +0200

    überarbeiteter Name für Create-Methode

--- src/org/hbrs/se1/ws24/exercises/uebung1/control/Factory.java
+++ src/org/hbrs/se1/ws24/exercises/uebung1/control/Factory.java
@@ -2,7 +2,7 @@ package org.hbrs.se1.ws24.exercises.uebung1.control;
 
 public class Factory {
 
-    public static GermanTranslator newGermanTranslator(String date){
+    public static GermanTranslator createTranslator(String date){
         GermanTranslator translator = new GermanTranslator();
         translator.setDate(date);
         return translator;
