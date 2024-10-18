4534eb4a4b39a5338951bdf0868ecbb80b61c28d
Emily.N <emily.nowakovki@smail.inf.h-brs.de>
2024-10-18 15:58:46 +0200

    erste Version Aufgabe3

--- src/org/hbrs/se1/ws24/exercises/uebung3/persistence/MemberView.java
+++ src/org/hbrs/se1/ws24/exercises/uebung3/persistence/MemberView.java
@@ -5,14 +5,9 @@ import org.hbrs.se1.ws24.solutions.uebung2.Member;
 import java.util.Iterator;
 
 public class MemberView {
-   public void dump(Container liste){
+   public void dump(Container liste) {
        System.out.println("Ausgabe aller Member-Objekte: ");
 
-       // Loesung Nr. 1 mit For each Schleife: Sequentielle Bearbeitung der Schleife
-       Iterator<Member> i = liste.iterator();
-       while (  i.hasNext() ) {
-           Member p = i.next();
-           System.out.println("ID: " + p.getID() );
-       }
+
    }
 }
