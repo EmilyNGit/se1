e7dea1eb3e70436f70cf028b3d29a2b07ffc5bd5
Emily.N <emily.nowakovki@smail.inf.h-brs.de>
2024-10-30 13:40:17 +0100

    Tests fuer Uebung4

--- test/org/hbrs/se1/ws24/tests/uebung4/VUSTest.java
+++ test/org/hbrs/se1/ws24/tests/uebung4/VUSTest.java
@@ -1,10 +1,9 @@
 package org.hbrs.se1.ws24.tests.uebung4;
 
 
-import org.hbrs.se1.ws24.exercises.uebung2.ConcreteMember;
-import org.hbrs.se1.ws24.exercises.uebung2.Container;
-import org.hbrs.se1.ws24.exercises.uebung2.ContainerException;
+import org.hbrs.se1.ws24.solutions.uebung3.*;
 import org.hbrs.se1.ws24.exercises.uebung4.UserStory;
+import org.hbrs.se1.ws24.solutions.uebung3.persistence.PersistenceException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,5 +11,66 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VUSTest {
+    private Container container;
+    private UserStory story1;
+    private UserStory story2;
+
+    @BeforeEach
+    public void setUp() {
+        container = Container.getInstance();
+        story1 = new UserStory(1, "UserStory1", "kriterium1", 2, 2, 3, 2, "Test");
+        story2 = new UserStory(2, "UserStory2", "kriterium2", 3, 2, 3, 1, "Test");
+    }
+
+    @Test
+    public void testAddMember() throws ContainerException {
+        try {
+            container.deleteAllObjects();
+        } catch (PersistenceException e) {
+        }
+        container.addObject(story1);
+        assertEquals(1, container.size());
+    }
+
+    @Test
+    public void testAddDuplicateMember() {
+        assertDoesNotThrow(() -> {
+            container.addObject(story1);
+        });
+        ContainerException thrown = assertThrows(ContainerException.class, () -> {
+            container.addObject(story1);
+        });
+        assertEquals(null, thrown.getMessage());
+    }
+
+
+    @Test
+    public void testDeleteMember() throws ContainerException {
+        try {
+            container.deleteAllObjects();
+        } catch (PersistenceException e) {
+        }
+        container.addObject(story1);
+        assertEquals("Member mit der ID 1 konnte geloescht werden", container.deleteObject(1));
+        assertEquals(0, container.size());
+    }
+
+    @Test
+    public void testDeleteNonExistentMember() {
+        assertEquals("Member nicht enthalten - ERROR", container.deleteObject(1));
+    }
+
+    @Test
+    public void testSize() throws ContainerException {
+        try {
+            container.deleteAllObjects();
+        } catch (PersistenceException e) {
+        }
+        assertEquals(0, container.size());
+        container.addObject(story1);
+        assertEquals(1, container.size());
+        container.addObject(story2);
+        assertEquals(2, container.size());
+    }
 
 }
