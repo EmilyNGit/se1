ad6452f5d4ba8985c9f245d0331c0772d1c7440e
Emily.N <emily.nowakovki@smail.inf.h-brs.de>
2024-10-30 12:12:14 +0100

    zweite Version Uebung4

---
+++ test/org/hbrs/se1/ws24/tests/uebung4/VUSTest.java
@@ -0,0 +1,16 @@
+package org.hbrs.se1.ws24.tests.uebung4;
+
+
+import org.hbrs.se1.ws24.exercises.uebung2.ConcreteMember;
+import org.hbrs.se1.ws24.exercises.uebung2.Container;
+import org.hbrs.se1.ws24.exercises.uebung2.ContainerException;
+import org.hbrs.se1.ws24.exercises.uebung4.UserStory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class VUSTest {
+
+}
